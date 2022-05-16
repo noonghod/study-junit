cc3418e13972494c7da12c1ff3dcad63b1ee85b9
noonghod <noonghod@gmail.com>
2022-05-16 21:41:43 +0900

    ch4

--- src/test/java/noonghod/study/junit/matching/ProfileTest.java
+++ src/test/java/noonghod/study/junit/matching/ProfileTest.java
@@ -40,4 +40,34 @@ class ProfileTest {
         assertTrue(matches);
     }
 
+    @Test
+    public void matches() {
+        Profile profile = new Profile("Bull Hockey, Inc.");
+        BooleanQuestion question = new BooleanQuestion(1, "Got milk?");
+
+        // must-match 항목이 맞지 않으면 false
+        profile.add(new Answer(question, Bool.FALSE_VALUE));
+        Criteria criteria = new Criteria();
+        criteria.add(
+                new Criterion(
+                        new Answer(question, Bool.TRUE_VALUE),
+                        Weight.MUST_MATCH
+                )
+        );
+
+        assertFalse(profile.matches(criteria));
+
+        // don't care 항목에 대해서는 true
+        profile.add(new Answer(question, Bool.FALSE_VALUE));
+        criteria = new Criteria();
+        criteria.add(
+                new Criterion(
+                        new Answer(question, Bool.TRUE_VALUE),
+                        Weight.DONT_CARE
+                )
+        );
+
+        assertTrue(profile.matches(criteria));
+    }
+
 }
