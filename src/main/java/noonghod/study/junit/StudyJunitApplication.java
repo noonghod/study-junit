5a8e532ee848150f248745d9b617426a30f3e4a2
noonghod <noonghod@gmail.com>
2022-05-16 21:09:00 +0900

    Initialize project

---
+++ src/main/java/noonghod/study/junit/StudyJunitApplication.java
@@ -0,0 +1,13 @@
+package noonghod.study.junit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJunitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyJunitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/noonghod/study/junit/StudyJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package noonghod.study.junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyJunitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
